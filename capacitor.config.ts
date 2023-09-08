03564501b062f4610c79880c86b4731ee2453084
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-09-08 12:57:50 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'metric_converter-yonathan_sihotang-ionic',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
