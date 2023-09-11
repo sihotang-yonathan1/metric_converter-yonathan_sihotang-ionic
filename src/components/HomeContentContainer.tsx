fa4faee6da6546dd96ee2793641eb8cb2308cdb0
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-09-11 22:09:36 +0800

    [error] show message when invalid input found

--- src/components/HomeContentContainer.tsx
+++ src/components/HomeContentContainer.tsx
@@ -400,7 +400,7 @@ export function ContentContainer(){
                     </div>
                 }
                 {
-                    isInvalidInput || isNaN(resultNumber)
+                    (isInvalidInput || isNaN(resultNumber))
                     && (
                         <div className="error_container">
                             <div>
